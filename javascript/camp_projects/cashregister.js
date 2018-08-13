a57ad51c6e0b7e02c5f39ec68c47f94f623a8b07
Dennis <vonalogue@gmail.com>
2018-08-13 15:45:13 -0700

    Update cashregister.js

--- javascript/camp_projects/cashregister.js
+++ javascript/camp_projects/cashregister.js
@@ -72,12 +72,26 @@ function checkCashRegister(price, cash, cid) {
     return change;
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], 
+                                         ["NICKEL", 0], 
+                                         ["DIME", 0], 
+                                         ["QUARTER", 0], ["ONE", 1], 
+                                         ["FIVE", 0], 
+                                         ["TEN", 0], 
+                                         ["TWENTY", 0], 
+                                         ["ONE HUNDRED", 0]]));
+// return {status: "INSUFFICIENT FUNDS", change: []}
 
-/*
-console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-
-//return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], 
+                                          ["NICKEL", 2.05], 
+                                          ["DIME", 3.1], 
+                                          ["QUARTER", 4.25], 
+                                          ["ONE", 90], 
+                                          ["FIVE", 55], 
+                                          ["TEN", 20], 
+                                          ["TWENTY", 60], 
+                                          ["ONE HUNDRED", 100]]));
+// return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
 
 console.log(checkCashRegister(30, 90, [ ['PENNY', 0],
                                         ['NICKEL', 0],
@@ -89,13 +103,15 @@ console.log(checkCashRegister(30, 90, [ ['PENNY', 0],
                                         ['TWENTY', 40.00],
                                         ['ONE HUNDRED', 100.00] ]));
 
-// change owed: $60
-// {status: "OPEN", change: [["TWENTY", 40], [["TEN", 20]]]};
+// return {status: "OPEN", change: [["TWENTY", 40], [["TEN", 20]]]};
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100] ]));
-
-
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
-*/
-
-// {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], 
+                                         ["NICKEL", 0], 
+                                         ["DIME", 0], 
+                                         ["QUARTER", 0], 
+                                         ["ONE", 0], 
+                                         ["FIVE", 0], 
+                                         ["TEN", 0], 
+                                         ["TWENTY", 0], 
+                                         ["ONE HUNDRED", 0]]));
+// return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
