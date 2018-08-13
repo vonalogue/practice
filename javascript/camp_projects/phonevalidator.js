7b219141e26ae41cd7702e95ab6a51d078a5d55d
Dennis <vonalogue@gmail.com>
2018-08-13 15:37:13 -0700

    Update phonevalidator.js

--- javascript/camp_projects/phonevalidator.js
+++ javascript/camp_projects/phonevalidator.js
@@ -1,9 +1,9 @@
 // Check whether a phone number is in valid US format. Inclusion of the country code is optional.
 
-    function telephoneCheck(str) {
-        const format = /^1?\s?(\d{3}|\(\d{3}\))[-\s]?\d{3}[-\s]?\d{4}$/;
-        return format.test(str);
-    }
+function telephoneCheck(str) {
+    const format = /^1?\s?(\d{3}|\(\d{3}\))[-\s]?\d{3}[-\s]?\d{4}$/;
+    return format.test(str);
+}
 
 console.log(telephoneCheck("1 555 555 5555"));          // return true
 console.log(telephoneCheck("3 555 555 5555"));          // return false
